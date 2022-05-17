03ca7c92abd27f74a4f54fc038f95b7d773da1c9
sidharthkumar2k19 <sidharthkumar2k19@gmail.com>
2022-05-17 23:29:26 +0530

    deploying

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,8 @@
-export const api = 'http://localhost:2000/api';
+export const baseURL = window.location.hostname == 'localhost' ?
+    'http://localhost:2000' :
+    'https://flipkartclone-rest-server.herokuapp.com'
+
+export const api = `${baseURL}/api`;
 export const generatePublicUrl = (fileName) => {
-    return `http://localhost:2000/public/${fileName}`;
+    return `${baseURL}/public/${fileName}`;
 }
